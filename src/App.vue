c0d48e6e6c34ba78251d9f8379cc5c092148f613
patty409 <yaqigood8@gmail.com>
2026-04-17 12:16:15 +0800

    Update currentTime assignment in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <h1>{{ currentTime }}</h1>
+  <div>
+    <h1>Current Time</h1>
+    <p>{{ currentTime }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 export default {
   setup() {
-    const currentTime = ref(new Date().toUTCString());
-    setInterval(() => {
-      currentTime.value = new Date().toUTCString();  // updated line
-    }, 1000);
+    const currentTime = ref('');
+    onMounted(() => {
+      currentTime.value = new Date().toLocaleTimeString();
+      setInterval(() => {
+        currentTime.value = new Date().toLocaleTimeString();
+      }, 1000);
+    });
     return { currentTime };
   }
 };
